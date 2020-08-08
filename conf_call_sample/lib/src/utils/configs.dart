244e182e5e1c0114a73862281fe7c8fac7f1c699
Ghulam Abbass <ghulamabbass.1995@gmail.com>
2020-08-08 19:11:20 +0500

    updated configs

--- conf_call_sample/lib/src/utils/configs.dart
+++ conf_call_sample/lib/src/utils/configs.dart
@@ -1,41 +1,86 @@
 import 'package:connectycube_sdk/connectycube_sdk.dart';
 
-const String APP_ID = "476";
-const String AUTH_KEY = "PDZjPBzAO8WPfCp";
-const String AUTH_SECRET = "6247kjxXCLRaua6";
-const String ACCOUNT_ID = "TpuBZox_HPxofh7PVZdP";
-const String DEFAULT_PASS = "xxasBUM3gQs36bhj";
+const String APP_ID = "2611";
+const String AUTH_KEY = "SNjfAhR-FwPJ8-F";
+const String AUTH_SECRET = "D49w2n9-fJFZdh7";
+
+const String ACCOUNT_ID = "V5MQ_EBhuTtSYTdc1V1r";
+const String DEFAULT_PASS = "12345678";
 const String SERVER_ENDPOINT = "wss://janus.connectycube.com:8989";
 
 List<CubeUser> users = [
   CubeUser(
-    id: 1253158,
-    login: "call_user_1",
+    id: 1500908,
+    login: "User1",
     fullName: "User 1",
     password: DEFAULT_PASS,
   ),
   CubeUser(
-    id: 1253159,
-    login: "call_user_2",
+    id: 1500909,
+    login: "User2",
     fullName: "User 2",
     password: DEFAULT_PASS,
   ),
   CubeUser(
-    id: 1253160,
-    login: "call_user_3",
+    id: 1500911,
+    login: "User3",
     fullName: "User 3",
     password: DEFAULT_PASS,
   ),
   CubeUser(
-    id: 1253162,
-    login: "call_user_4",
+    id: 1500912,
+    login: "User4",
     fullName: "User 4",
     password: DEFAULT_PASS,
   ),
   CubeUser(
-    id: 1425506,
-    login: "call_user_5",
+    id: 1500913,
+    login: "User5",
     fullName: "User 5",
     password: DEFAULT_PASS,
   ),
+  CubeUser(
+    id: 1500914,
+    login: "User6",
+    fullName: "User 6",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 1500915,
+    login: "User7",
+    fullName: "User 7",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 1500916,
+    login: "User8",
+    fullName: "User 8",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 1500917,
+    login: "User9",
+    fullName: "User 6",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 1500918	,
+    login: "User10",
+    fullName: "User 10",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 1758581	,
+    login: "User11",
+    fullName: "User 11",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 1758587	,
+    login: "User12",
+    fullName: "User 12",
+    password: DEFAULT_PASS,
+  ),
+
+  
 ];
